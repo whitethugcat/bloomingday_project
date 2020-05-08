3c18fa1d4bc6b71aa6c22118d4ded52ed70f8f75
Ubuntu <ubuntu@ip-172-31-42-114.ap-northeast-2.compute.internal>
2020-05-08 14:31:31 +0000

    0508 backend-1

---
+++ app/admin/sellers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Seller do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :img, :region, :sort, :location, :time, :close, :contact, :delivery
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :img, :region, :sort, :location, :time, :close, :contact, :delivery]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
